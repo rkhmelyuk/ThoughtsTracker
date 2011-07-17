6579f4af6ecac27a4fa9617b9209799152eb6cb6
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2011-07-17 12:22:40 +0300

    Fix issue with font size where max = 1.

--- thought/templatetags/tagfilters.py
+++ thought/templatetags/tagfilters.py
@@ -2,8 +2,16 @@ from django import template
 
 register = template.Library()
 
+MIN_FONT_SIZE = 0.9
+
 @register.filter(name="fontsize")
 def asFontSizeEm(num, max):
-    percent = float(num - 1) / (max - 1)
-    fontSize = 0.9 + 0.6 * percent
-    return str(fontSize) + "em"
+    if max > 1:
+        percent = float(num - 1) / (max - 1)
+        fontSize = MIN_FONT_SIZE + 0.6 * percent
+        return toFontSize(fontSize)
+
+    return toFontSize(MIN_FONT_SIZE)
+
+def toFontSize(size):
+    return str(size) + "em"
